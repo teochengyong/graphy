69f09d3fce1ae281bb8367aab41d51aaba9bf1e6
chengyong <teochengyong@gmail.com>
2017-04-03 09:23:40 +0800

    Creating a bar graph programmatically

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,26 @@
 (function (w) {
-  const app = {
-    views: {}
-  }
-  const el = 'bar'
+  const inputSelector = 'input'
+  const inputEl = document.getElementById(inputSelector)
+  const airportPostcode = 819634
+  inputEl.addEventListener('change', (e) => {
+    const fr = new window.FileReader()
+    let data
+    fr.onloadend = (e) => {
+      const text = fr.result
+      data = text.split('\n')
+      data = data.filter((d) => d.length === 6)
+      console.log(data)
+    }
 
+    if (e.target.files.length > 0) {
+      fr.readAsText(e.target.files[0], 'UTF-8')
+    }
+  }, false)
+  const barData = [4, 8, 15, 16, 23, 42]
+  d3.select('.chart')
+        .selectAll('div')
+        .data(barData)
+        .enter().append('div')
+        .style('width', function (d) { return d * 10 + 'px' })
+        .text(function (d) { return d })
 })(window)
