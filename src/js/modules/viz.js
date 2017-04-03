90d0e772aa2456d9f956f504cbabb7c0081cf65e
chengyong <teochengyong@gmail.com>
2017-04-03 09:23:13 +0800

    Fixed linting

--- src/js/modules/viz.js
+++ src/js/modules/viz.js
@@ -1,20 +1,19 @@
 (function () {
-  "use strict";
+  'use strict'
   const data = [4, 8, 15, 16, 23, 42]
   const root = this
   const previous = root.Viz
 
-  let has_require = typeof require !== 'undefined'
+  let hasRequire = typeof require !== 'undefined'
   let d3 = root.d3
 
-  if(typeof d3 === 'undefined') {
-    if(has_require) {
+  if (typeof d3 === 'undefined') {
+    if (hasRequire) {
       d3 = require('d3')
     } else throw new Error('Viz requires d3, see `"https://d3js.org/`"')
   }
 
-
-  const Viz = function Viz() {}
+  const Viz = function Viz () {}
 
   Viz.prototype.d3 = d3
   Viz.prototype.noConflict = function () {
@@ -23,18 +22,15 @@
   }
 
   Viz.prototype.draw = function (type, options) {
-    return
+
   }
 
   Viz.prototype.transform = function (data, options) {
-    return
+
   }
 
-
-
-
-  if(typeof exports !== 'undefined') {
-    if( typeof module !== 'undefined' && module.exports) {
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
       module.exports = Viz
     }
     exports.Viz = Viz
