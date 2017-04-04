42475744058c81bb0de7ae7cd99f50ae45fd9935
chengyong <teochengyong@gmail.com>
2017-04-04 09:22:42 +0800

    Generating bar chart with svg

--- src/js/app.js
+++ src/js/app.js
@@ -17,10 +17,27 @@
     }
   }, false)
   const barData = [4, 8, 15, 16, 23, 42]
-  d3.select('.chart')
-        .selectAll('div')
+  const width = 420
+  const barHeight = 20
+  let x = window.d3.scaleLinear()
+          .domain([0, window.d3.max(barData)])
+          .range([0, width])
+
+  let chart = window.d3.select('.chart')
+        .attr('width', width)
+        .attr('height', barHeight * barData.length)
+  let bar = chart
+        .selectAll('g')
         .data(barData)
-        .enter().append('div')
-        .style('width', function (d) { return d * 10 + 'px' })
-        .text(function (d) { return d })
+        .enter().append('g')
+        .attr('transform', function (d, i) { return `translate(0,` + i * barHeight + `)` })
+  bar.append('rect')
+      .attr('width', x)
+      .attr('height', barHeight - 1)
+
+  bar.append('text')
+      .attr('x', function (d) { return x(d) - 3 })
+      .attr('y', barHeight / 2)
+      .attr('dy', '.35em')
+      .text(function (d) { return d })
 })(window)
